642fc4db671a95018e248ede5a3368a5c317695e
gabriilla87 <gabriilla87@gmail.com>
2024-09-16 16:56:43 +0300

    9-th homework is done

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -11,10 +11,10 @@ import s2 from '../../s1-main/App.module.css'
 
 const HW9 = () => {
     return (
-        <div id={'hw9'}>
+        <div id={'hw9'} style={{marginTop: "36px"}}>
             <div className={s2.hwTitle}>Homework #9</div>
 
-            <div className={s2.hw}>
+            <div className={s2.hw} style={{paddingTop: "26px"}}>
                 <Clock />
             </div>
         </div>
